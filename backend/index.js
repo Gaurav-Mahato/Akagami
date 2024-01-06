fb9a9a4aedef504360e9f9f41249d0dc2417ca98
GauravMahato <Gauravmahato1309@gmail.com>
2024-01-07 03:10:33 +0530

    Established Database collection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
+const connectDB = require('./config/db.js')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+connectDB()
 
 const port = process.env.PORT || 4000
 
@@ -8,6 +16,6 @@ app.get("/",(req,res) => {
     res.send("Hello World")
 })
  
-app.listen({port:PORT},async() => {
-    console.log(`Server running on port ${PORT}`)
+app.listen({port:4000},async() => {
+    console.log(`Server running on port 4000`)
 })
