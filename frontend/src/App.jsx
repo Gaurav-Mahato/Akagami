c10db0263ce1648aac07b0da435fcbc5098ff3b1
Nived Singh <s.nived@iitg.ac.in>
2024-01-08 16:34:35 +0530

    Added Chat Area, Groups and Login functionallity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import MainContainer from "./Components/MainContainer";
+import Login from "./Components/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import ChatArea from "./Components/ChatArea";
+import Users from "./Components/Users";
+import CreateGroups from "./Components/CreateGroups";
+import Groups from "./Components/Groups";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state) => state.themeKey);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
